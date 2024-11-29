69227ca93d2b943c505c66570b4dea7d02521359
speedy005 <67871007+speedy005@users.noreply.github.com>
2024-11-29 16:53:41 +0100

    Update globals.h

--- globals.h
+++ globals.h
@@ -378,12 +378,12 @@ typedef uint8_t uint8_t;
  *         constants
  * =========================== */
 #define SCM_URL					"https://github.com/speedy005/Ncam"
-#define CS_VERSION    "_bonecrew_r1.0.5_2.24.11_11856_"
+#define CS_VERSION    "_bonecrew_r1.0.6_2.24.11_11856_"
 #ifndef CS_REVISION
 #define CS_REVISION   "_bonecrew_"
 #endif
 #ifndef CS_GIT_VERSION
-#define CS_GIT_VERSION "r1.0.5"
+#define CS_GIT_VERSION "r1.0.6"
 #endif
 #ifndef CS_DATE_BUILD
 #define CS_DATE_BUILD  ""
