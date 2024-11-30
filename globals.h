0b4729f1dfb6e1c6bdd4c912450f08ca324f8bf1
speedy005 <67871007+speedy005@users.noreply.github.com>
2024-11-30 20:41:47 +0100

    Update globals.h

--- globals.h
+++ globals.h
@@ -380,7 +380,7 @@ typedef uint8_t uint8_t;
 #define SCM_URL					"https://github.com/speedy005/Ncam/commit/master"
 #define WIKI_URL				"https://wiki.streamboard.tv/wiki"
 #define BOARD_URL				"https://board.streamboard.tv"
-#define CS_VERSION    "_bonecrew-r1.0.6-2024-11-11856_"
+#define CS_VERSION    "_bonecrew_r1.0.6_2024_11_11856_"
 #ifndef CS_REVISION
 #define CS_REVISION   "_bonecrew_"
 #endif
