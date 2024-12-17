d801b0bfa935d601828fc5bc3f67de6290494be5
speedy005 <67871007+speedy005@users.noreply.github.com>
2024-12-17 17:53:25 +0100

    Update ncam.c

--- ncam.c
+++ ncam.c
@@ -158,8 +158,9 @@ static void show_usage(void)
 	}
 	printf("\n");
 	printf("Copyright (C) 2012-2018 developed by Javilonas.\n");
-	printf("Copyright (C) 2016-2024 developed by RAED.\n");
-	printf("Visit https://www.tunisia-sat.com/ for more details.\n\n");
+	printf("Copyright (C) 2012-2018 developed by READ.\n");
+	printf("2024 Updated, Patched and Recompiled by bonecrew.\n");
+	printf("Visit https://bone-crew.neocities.org/for more details.\n\n");
 
 	printf(" ConfigDir  : %s\n", CS_CONFDIR);
 	printf("\n");
@@ -383,43 +384,60 @@ static void write_versionfile(bool use_stdout)
 		struct tm st;
 		time_t walltime = cs_time();
 		localtime_r(&walltime, &st);
-		fprintf(fp, "Unix Starttime: %ld\n", walltime);
-		fprintf(fp, "Starttime:      %02d.%02d.%04d %02d:%02d:%02d\n",
+		fprintf(fp, "Unix Starttime:    %ld\n", walltime);
+		fprintf(fp, "Starttime:         %02d.%02d.%04d %02d:%02d:%02d\n",
 				st.tm_mday, st.tm_mon + 1, st.tm_year + 1900,
 				st.tm_hour, st.tm_min, st.tm_sec);
 	}
-	fprintf(fp, "Build Date:     %s\n", CS_BUILD_DATE);
-#ifdef MODULE_STREAMRELAY
-	fprintf(fp, "Version:        NCam-%s-StreamRelay\n", CS_VERSION);
-#else
-	fprintf(fp, "Version:        NCam-%s\n", CS_VERSION);
-#endif
-	fprintf(fp, "Revision:       %s\n", CS_REVISION);
-	fprintf(fp, "Build:          %s\n", CS_DATE_BUILD);
+	fprintf(fp,   "Build Date:        %s\n", CS_BUILD_DATE);
+fprintf(fp,   "Version:           %s@%s\n", CS_VERSION,  "-with Streamrelay build by bonecrew");
+		fprintf(fp, "Compiled by:       %s\n", "Simplebuild 3 bonecrew");
+	        fprintf(fp, "S3 mit:            -ggdb -pipe -ffunction-sections -fdata-sections -fomit-frame-pointer -fno-schedule-insns \n");
+		fprintf(fp, "Copyright:         %s\n", "by bonecrew 2024");
+	        fprintf(fp, "compiler_option    -O2   \n");
+		fprintf(fp, "compress           nein   \n");
+		fprintf(fp, "Toolchain          armv7a by bonecrew \n");
+		fprintf(fp, "Bibliothek         LIB_SSL 3.4.0 \n");
+		fprintf(fp, "Bibliothek         LIB_USB 1.0.27 \n");
+		fprintf(fp, "Bibliothek         LIB_PCSC 2.3.0 \n");
+        fprintf(fp, "Bibliothek         LIB_ZLIB 1.3.1 \n");
+        fprintf(fp, "Bibliothek         LIB_DVBCSA 1.1.0 \n");
+        fprintf(fp, "Bibliothek         LIB_CURL 8.11.0 \n");
+        fprintf(fp, "Bibliothek         LIB_CCID 1.6.1 \n");
+        fprintf(fp, "Options            webif-dvbapi-ssl-libusb-pcsc-libdvbcsa-streamrelay-neon-extra \n");
+        fprintf(fp, "Patch              nein \n");
+		fprintf(fp, "Patch              nein \n");
+		fprintf(fp, "Patch              nein \n");
+		fprintf(fp, "Neon               ja / 128 \n");
+ 	    fprintf(fp, "TempDir:           %s\n", cs_tmpdir);
+ 	    fprintf(fp, "Box type:          %s (%s)\n", boxtype_get(), boxname_get());
+ 	    fprintf(fp, "PID:               %d\n", getppid());
+        fprintf(fp, "Revision:          %s\n", CS_REVISION);
+	    fprintf(fp, "Build:             %s\n", CS_DATE_BUILD);
 #ifdef CS_CACHEEX_AIO
-	fprintf(fp, "Cache exchange: %s\n", CS_AIO_VERSION);
+	fprintf(fp,   "Cache exchange:    %s\n", CS_AIO_VERSION);
 #endif
-	fprintf(fp, "Compiler:       %s\n", CS_TARGET);
+	fprintf(fp,   "Compiler:          %s\n", CS_TARGET);
 #ifdef USE_COMPRESS
-	fprintf(fp, "Compression:    %s, level %s\n", COMP_VERSION, COMP_LEVEL);
+	fprintf(fp,   "Compression:       %s, level %s\n", COMP_VERSION, COMP_LEVEL);
 #endif
-	fprintf(fp, "Box Type:       %s (%s)\n", boxtype_get(), boxname_get());
-	fprintf(fp, "PID:            %d\n", getppid());
-	fprintf(fp, "TempDir:        %s\n", cs_tmpdir);
+	fprintf(fp,   "Box Type:          %s (%s)\n", boxtype_get(), boxname_get());
+	fprintf(fp,   "PID:               %d\n", getppid());
+	fprintf(fp,   "TempDir:           %s\n", cs_tmpdir);
 #ifdef MODULE_GBOX
 	if(cfg.gbox_tmp_dir == NULL)
 	{
-		fprintf(fp, "GBox tmp_dir:   not defined using: %s\n", cs_tmpdir);
+		fprintf(fp,   "GBox tmp_dir:      not defined using: %s\n", cs_tmpdir);
 	}
 	else
 	{
-		fprintf(fp, "GBox tmp_dir:   %s\n", cfg.gbox_tmp_dir);
+		fprintf(fp,   "GBox tmp_dir:      %s\n", cfg.gbox_tmp_dir);
 	}
 #endif
 
-	fprintf(fp, "ConfigDir:      %s\n", cs_confdir);
+	fprintf(fp,      "ConfigDir:         %s\n", cs_confdir);
 #ifdef WEBIF
-	fprintf(fp, "WebifPort:      %d\n", cfg.http_port);
+	fprintf(fp,      "WebifPort:         %d\n", cfg.http_port);
 #endif
 
 #ifdef WITH_SIGNING
